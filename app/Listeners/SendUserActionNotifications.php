e88adbd296f316846375b3262084837520d3d0f0
DjStar7 <infodjstar7@gmail.com>
2025-11-30 09:50:09 +0100

    Finalisation du projet

--- app/Listeners/SendUserActionNotifications.php
+++ app/Listeners/SendUserActionNotifications.php
@@ -3,9 +3,11 @@
 namespace App\Listeners;
 
 use App\Events\UserActionEvent;
+use App\Models\User;
 use App\Notifications\UserActionNotification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
 
 class SendUserActionNotifications
 {
@@ -23,8 +25,35 @@ class SendUserActionNotifications
 
     public function handle(UserActionEvent $event)
     {
-        $user = $event->user;
+        $userData = $event->user;
+        // Si $userData est déjà un User (parfois Laravel fait ça)
+        if ($userData instanceof User) {
+            $user = $userData;
+        }
 
-        $user->notify(new UserActionNotification($event->payload));
+        // Si c’est un tableau contenant un User sérialisé
+        elseif (is_array($userData) && count($userData) > 0) {
+            $first = reset($userData);
+
+            // cas où on a ["App\Models\User" => ["id" => "..."]]
+            if (is_array($first)) {
+                $userId = $first['id'] ?? null;
+            } else {
+                $userId = $userData['id'] ?? null;
+            }
+
+            // Si $userData est déjà un User (parfois
+
+            $user = $userId ? User::find($userId) : null;
+        } else {
+            Log::error("UserActionEvent user invalid", [$userData]);
+            return;
+        }
+
+        if ($user) {
+            $user->notify(new UserActionNotification($event->payload));
+        } else {
+            Log::error("Impossible de récupérer le user dans SendUserActionNotifications", [$userData]);
+        }
     }
 }
